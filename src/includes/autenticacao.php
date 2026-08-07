78138a30c21d7253bff97de06cc3ed68552908f4
giovana_mendonca <giovanamendoncaraimundo@gmail.com>
2026-08-07 14:36:25 -0300

    feat: criação das autenticações que devem ser feitas com os usuários

---
+++ src/includes/autenticacao.php
@@ -0,0 +1,20 @@
+<?php
+
+// Verifica se o usuário possui uma sessão ativa
+if(!isset($_SESSION['id'])){
+    header("Location: ../auth/login.php");
+    exit();
+}
+
+// Verifica se o usuário tem o nível minímo para acessar
+function nivelMinimo($nivel){
+    return $_SESSION['nivel'] >= $nivel;
+}
+
+// Define o nível de permissão para poder acessar
+function requireNivel($nivel){
+    if(!nivelMinimo($nivel)){
+        die("Acesso negado.");
+    }
+}
+?>
